efa350866cc9f2850bf1c3eb23a3e7be8729a451
umerFarooki <umerfarooqi@protonmail.com>
2021-03-02 02:21:09 +0300

    unit tests added

--- ArticleBlogTests/ArticleBlogTests.swift
+++ ArticleBlogTests/ArticleBlogTests.swift
@@ -10,9 +10,16 @@ import XCTest
 @testable import ArticleBlog
 
 class ArticleBlogTests: XCTestCase {
+    
+    var viewControllerUnderTest: ArticlesViewController!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        self.viewControllerUnderTest = storyboard.instantiateViewController(withIdentifier: "ArticleVC") as? ArticlesViewController
+        self.viewControllerUnderTest.loadView()
+        self.viewControllerUnderTest.viewDidLoad()
     }
 
     override func tearDown() {
@@ -30,5 +37,30 @@ class ArticleBlogTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func testHasATableView() {
+         XCTAssertNotNil(viewControllerUnderTest.tableView)
+     }
+     func testTableViewHasDelegate() {
+         XCTAssertNotNil(viewControllerUnderTest.tableView.delegate)
+     }
+     func testTableViewConfromsToTableViewDelegateProtocol() {
+         XCTAssertTrue(viewControllerUnderTest.conforms(to: UITableViewDelegate.self))
+         XCTAssertTrue(viewControllerUnderTest.responds(to: #selector(viewControllerUnderTest.tableView(_:didSelectRowAt:))))
+     }
+     func testTableViewHasDataSource() {
+         XCTAssertNotNil(viewControllerUnderTest.tableView.dataSource)
+     }
+     func testTableViewConformsToTableViewDataSourceProtocol() {
+         XCTAssertTrue(viewControllerUnderTest.conforms(to: UITableViewDataSource.self))
+         XCTAssertTrue(viewControllerUnderTest.responds(to: #selector(viewControllerUnderTest.tableView(_:numberOfRowsInSection:))))
+         XCTAssertTrue(viewControllerUnderTest.responds(to: #selector(viewControllerUnderTest.tableView(_:cellForRowAt:))))
+     }
+     func testTableViewCellHasReuseIdentifier() {
+         let cell = viewControllerUnderTest.tableView(viewControllerUnderTest.tableView, cellForRowAt: IndexPath(row: 0, section: 0)) as? ArticleTableViewCell
+         let actualReuseIdentifer = cell?.reuseIdentifier
+         let expectedReuseIdentifier = "ArticleTableViewCell"
+         XCTAssertEqual(actualReuseIdentifer, expectedReuseIdentifier)
+     }
 
 }
